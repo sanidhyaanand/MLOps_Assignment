161c48697b4eea10c34bd375c16dfd47be8b8eec
sanidhyaanand <sanidhya.anand03@gmail.com>
2021-07-15 20:25:53 +0530

    results

--- src/expt.py
+++ src/expt.py
@@ -1,8 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[54]:
+
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import dvc.api
 from sklearn.model_selection import train_test_split
+from sklearn import tree, metrics
+import pickle
+import json
+
+
+# In[2]:
+
 
 # reading dataset
 with dvc.api.open("data/creditcard.csv", repo = "https://github.com/sanidhyaanand/MLOps_Assignment") as fd:
@@ -13,4 +26,64 @@ with dvc.api.open("data/creditcard.csv", repo = "https://github.com/sanidhyaanan
 # X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.4,random_state=42, stratify=Y) 
 
 x = dvc.api.get_url(repo="https://github.com/sanidhyaanand/MLOps_Assignment", path="data/creditcard.csv")
-print(x)
+print("metadata points to:" + x)
+
+
+# In[12]:
+
+
+# df.head()
+
+
+# In[18]:
+
+
+y = df.Class
+X = df.drop(["Class", "Time"], axis = 1)
+
+
+# In[40]:
+
+
+#test-train split (80-20)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42,stratify=y)
+
+train_df = pd.concat([X_train, y_train], axis=1)
+test_df = pd.concat([X_test, y_test], axis=1)
+
+train_df.to_csv("../data/processed/train.csv")
+test_df.to_csv("../data/processed/test.csv")
+
+
+# In[48]:
+
+
+# training and prediction step
+clf = tree.DecisionTreeClassifier(criterion = "entropy")
+clf = clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+
+# In[49]:
+
+
+# saving model
+filename = "../models/model.pkl"
+pickle.dump(clf, open(filename, 'wb'))
+
+
+# In[56]:
+
+
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+print("Weighted F1 Score:", metrics.f1_score(y_test, y_pred, average = 'weighted'))
+
+results = "Accuracy: " +  str(metrics.accuracy_score(y_test, y_pred)) + "\n" + "Weighted F1 Score:" + str(metrics.f1_score(y_test, y_pred, average = 'weighted'))
+
+
+# In[57]:
+
+
+with open('../metrics/acc_f1.json', 'w', encoding='utf-8') as f:
+    json.dump(results, f, ensure_ascii=False, indent=4)
+
